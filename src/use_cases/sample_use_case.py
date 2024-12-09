4a6651e37f0d7bad5cdedbe4b586548ed292950b
alrza <alrza.msvnj@gmail.com>
2024-12-09 11:23:00 +0330

    add database connection and base classes

--- src/use_cases/sample_use_case.py
+++
@@ -1,5 +0,0 @@
-class SubmitBidUseCase:
-    def __init__(self, auction_repository: AuctionRepository):
-        self._auction_repository = auction_repository
-
-    async def __call__(self, bid: Bid) -> None: ...
